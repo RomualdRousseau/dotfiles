29b5d83eda7e7fb8dd39954f8bd282a3bfa5b71f
Romuald Rousseau <romualdrousseau@gmail.com>
2025-07-14 15:11:18 +0200

    feat: align explorer keymap with vim, nvim and vscode

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,6 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal toggle<CR>", {})
+    vim.keymap.set("n", "<C-S-E>", ":Neotree filesystem reveal toggle<CR>", {})
   end,
 }
