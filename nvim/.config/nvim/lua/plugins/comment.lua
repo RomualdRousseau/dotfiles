a311887936f8b384c9f176336541ef2de95b9151
Romuald Rousseau <romualdrousseau@gmail.com>
2025-10-20 07:51:43 +0200

    feat: add new keyset

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,20 +1,25 @@
 return {
-  "numToStr/Comment.nvim",
-  config = function()
-    require("Comment").setup({})
+	"numToStr/Comment.nvim",
+	config = function()
+		require("Comment").setup({})
 
-    local api = require("Comment.api")
-    vim.keymap.set("n", "<C-_>", api.toggle.linewise.current)
-    vim.keymap.set("n", "<C-\\>", api.toggle.blockwise.current)
+		local api = require("Comment.api")
+		vim.keymap.set("n", "<C-_>", api.toggle.linewise.current)
+		vim.keymap.set("n", "<C-/>", api.toggle.linewise.current)
+		vim.keymap.set("n", "<C-\\>", api.toggle.blockwise.current)
 
-    local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
-    vim.keymap.set("x", "<C-_>", function()
-      vim.api.nvim_feedkeys(esc, "nx", false)
-      api.toggle.linewise(vim.fn.visualmode())
-    end)
-    vim.keymap.set("x", "<C-\\>", function()
-      vim.api.nvim_feedkeys(esc, "nx", false)
-      api.toggle.blockwise(vim.fn.visualmode())
-    end)
-  end,
+		local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+		vim.keymap.set("x", "<C-_>", function()
+			vim.api.nvim_feedkeys(esc, "nx", false)
+			api.toggle.linewise(vim.fn.visualmode())
+		end)
+		vim.keymap.set("x", "<C-/>", function()
+			vim.api.nvim_feedkeys(esc, "nx", false)
+			api.toggle.linewise(vim.fn.visualmode())
+		end)
+		vim.keymap.set("x", "<C-\\>", function()
+			vim.api.nvim_feedkeys(esc, "nx", false)
+			api.toggle.blockwise(vim.fn.visualmode())
+		end)
+	end,
 }
