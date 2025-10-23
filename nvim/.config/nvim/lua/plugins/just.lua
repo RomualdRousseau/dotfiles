559b665f0312a87a716285d1647b3cd3699f5e9f
Romuald Rousseau <romualdrousseau@gmail.com>
2025-10-23 08:31:52 +0200

    fix: change just plugin

--- nvim/.config/nvim/lua/plugins/just.lua
+++ nvim/.config/nvim/lua/plugins/just.lua
@@ -1,11 +1,20 @@
+-- return {
+--   "BeatScherrer/just.nvim",
+--   dependencies = {
+--     "nvim-telescope/telescope.nvim",
+--   },
+--   config = function()
+--     require("just").setup()
+--     require("telescope").load_extension("just")
+--     vim.keymap.set("n", "<leader>fj", require("telescope.recipePicker").recipePicker, {})
+--   end,
+-- }
 return {
-  "BeatScherrer/just.nvim",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-  },
-  config = function()
-    require("just").setup()
-    require("telescope").load_extension("just")
-    vim.keymap.set("n", "<leader>fj", require("telescope.recipePicker").recipePicker, {})
-  end,
+	"al1-ce/just.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+		"al1-ce/jsfunc.nvim",
+	},
+	config = true,
 }
