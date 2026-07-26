e80a95d3ab40ed27ab3f967c5762799e0d8ac649
Romuald Rousseau <romualdrousseau@gmail.com>
2026-07-26 16:15:22 +0200

    chore: update nvim plugins

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,34 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
+  branch = "main",
   lazy = false,
+  build = ":TSUpdate",
+
   config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "lua", "python", "java" },
-      highlight = { enable = true },
-      indent = { enable = true },
+    local treesitter = require("nvim-treesitter")
+
+    local languages = {
+      "lua",
+      "python",
+      "java",
+      "markdown",
+      "toml",
+      "yaml",
+      "json",
+      "just",
+    }
+
+    treesitter.install(languages)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = languages,
+      callback = function(args)
+        local language = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+
+        if language and pcall(vim.treesitter.start, args.buf, language) then
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
     })
   end,
 }
