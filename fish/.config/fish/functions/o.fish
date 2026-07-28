ba1f5cb8025ca39bfbbb37cf3bf0f2d65444bb4e
Romuald Rousseau <romualdrousseau@gmail.com>
2026-07-28 16:39:22 +0200

    feat: update treesitter config and add o fish helper

---
+++ fish/.config/fish/functions/o.fish
@@ -0,0 +1,4 @@
+function o --wraps='GOOGLE_CLOUD_PROJECT=gemini-code-assist-7412 GOOGLE_APPLICATION_CREDENTIALS=~/.config/gcloud/application_default_credentials.json VERTEX_LOCATION=global EDITOR=nvim opencode' --description 'alias o=GOOGLE_CLOUD_PROJECT=gemini-code-assist-7412 GOOGLE_APPLICATION_CREDENTIALS=~/.config/gcloud/application_default_credentials.json VERTEX_LOCATION=global EDITOR=nvim opencode'
+  GOOGLE_CLOUD_PROJECT=gemini-code-assist-7412 GOOGLE_APPLICATION_CREDENTIALS=~/.config/gcloud/application_default_credentials.json VERTEX_LOCATION=global EDITOR=nvim opencode $argv
+        
+end
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,50 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "main",
-  lazy = false,
-  build = ":TSUpdate",
+if vim.fn.has("nvim-0.12") == 1 then
+  return {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
 
-  config = function()
-    local treesitter = require("nvim-treesitter")
+    config = function()
+      local treesitter = require("nvim-treesitter")
 
-    local languages = {
-      "lua",
-      "python",
-      "java",
-      "markdown",
-      "toml",
-      "yaml",
-      "json",
-      "just",
-    }
+      local languages = {
+        "lua",
+        "python",
+        "java",
+        "markdown",
+        "toml",
+        "yaml",
+        "json",
+        "just",
+      }
 
-    treesitter.install(languages)
+      treesitter.install(languages)
 
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = languages,
-      callback = function(args)
-        local language = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = languages,
+        callback = function(args)
+          local language = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
 
-        if language and pcall(vim.treesitter.start, args.buf, language) then
-          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end
-      end,
-    })
-  end,
-}
+          if language and pcall(vim.treesitter.start, args.buf, language) then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
+  }
+else
+  return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = false,
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = { "lua", "python", "java" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  }
+end
